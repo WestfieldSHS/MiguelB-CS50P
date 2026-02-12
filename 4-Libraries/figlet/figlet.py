9211fbee579d48fcc262a3f9b49d997c984e8b06
MiguelB5119 <miguel.beira387@education.nsw.gov.au>
2026-02-13 10:40:00 +1100

    figlet - completed in my own way

--- 4-Libraries/figlet/figlet.py
+++ 4-Libraries/figlet/figlet.py
@@ -1,9 +1,7 @@
-# i don't know how figlet works.
-import random
+import random, sys
 from pyfiglet import Figlet
 figlet = Figlet()
 font_list = figlet.getFonts()
-
 decision = False
 
 text = input("Text: ")
@@ -11,8 +9,13 @@ font_decision = input("Do you want to set the font? Y/N: ")
 while decision == False:
     if font_decision == "Y":
         f = input("What font do you want? ").lower()
-        decision = True
+        if f in font_list:
+            decision = True
+        else:
+            print("Invalid Usage")
+            sys.exit()
     elif font_decision == "N":
         f = random.choice(font_list)
-figlet.setFont(font=f)
-print(figlet.renderText(text))
+        decision = True
+f = Figlet(font=f)
+print(f.renderText(text))
